4b6b8973ef41f57261a177a26d4740ed9b5b6e9d
Sangeetha Sasikumar <sangeekumar7@gmail.com>
2018-02-04 23:46:48 -0500

    Add files via upload

---
+++ Basics.r
@@ -0,0 +1,13 @@
+counter <- 1
+while(counter <12){
+  print(counter)
+  counter <- counter +1
+}
+x<-5
+x
+for(i in 1:5){    # prints out 5
+  print("Hello")
+}
+for(i in 0:5){    #prints out 6
+  print("Hello")
+}
