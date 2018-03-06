9cbff1ae66f8403dbcf2df1f8e7a9d6ef667b07d
Sangeetha Sasikumar <sangeekumar7@gmail.com>
2018-03-06 13:13:38 -0500

    Update DeletingNA.r

--- DeletingNA.r
+++ DeletingNA.r
@@ -1,11 +1,15 @@
 library(readxl)
-
+library(reshape2)
+library(ggplot2)
+library(plotly)
+library(lubridate)
+library(dplyr)
 setwd("/Users/Sangeetha/Downloads/")
 
 
 actuaries <- read_excel("Actuaries-Climate-Index-Values-Through-May-2017_Spring-2017_English.xlsx", sheet = 2)
 
-actuaries <- actuaries[, -1]
+#actuaries <- actuaries[, -1]
 actuaries <- na.omit(actuaries)
 
 #two data structures {years, months}
@@ -13,10 +17,12 @@ actuaries <- na.omit(actuaries)
 #concatenation in inner loop
 
 years <- 1961:2017
-months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+months <- 1:12
+
+#format(Sys.time(), "%a %b %d %X %Y")
 
 
-field_names <- c()
+field_names <- c("Sea_Level_Monthly")
 
 for(i in 1:length(years)){
   
@@ -24,7 +30,10 @@ for(i in 1:length(years)){
   
   for(j in 1:length(months)){
     
-    tmp <- paste0(months[j], "_", years[i])
+    #date tmp in YYYY-MM-DD
+    tmp <- paste0(years[i], "-", months[j], "-1")
+    
+    
     #print(tmp)
     
     field_names <- c(field_names, tmp)
@@ -34,8 +43,68 @@ for(i in 1:length(years)){
 }
 
 #trim the last 7 months
-
 field_names <- field_names[1:(length(field_names)-7)]
 
 colnames(actuaries) <- field_names
+
+
+actuaries_part1 <- actuaries[4,]
+actuaries_part1 <- melt(actuaries_part1, id=c("Sea_Level_Monthly"))
+
+actuaries_part1 <- actuaries_part1[, -1]
+names(actuaries_part1) <- c("month_date", "sea_level")
+actuaries_part1$month_date <- as.Date(actuaries_part1$month_date)
+
+plot1 <- ggplot(data = actuaries_part1, aes(x = month_date, y = sea_level)) + geom_line()
+
+ggplotly(plot1)
+
+View(actuaries_part1)
+
+
+
+#sectionning in time series
+#facetting
+actuaries_part1$year <- format(actuaries_part1$month_date, "%Y")
+
+#interval must be years
+interval = 10
+
+#lets create a section column which has the same value for all years in the same interval.
+#intervals being [1961-1970], [1971, 1980], etc..
+
+actuaries_part1$year <- as.integer(actuaries_part1$year)
+actuaries_part1 <- actuaries_part1 %>% filter(year <= 2010)
+
+sections <- actuaries_part1$year %>% unlist() %>% c()
+
+interval <- interval*12
+
+sections <- c()
+
+i <- 1
+j <- 1
+
+for(i in 1:nrow(actuaries_part1)){
+  sections <- c(sections, j)
+
+  if( (i%%interval) == 0)
+  j <- j + 1
+    
+  i <- i + 1 
+  
+}
+
+actuaries_part1$section <- sections
+actuaries_part1$section <- paste("Section", actuaries_part1$section)
+
+
+plot_data <- actuaries_part1[, c("sea_level", "section", "month_date")]
+
+
+sectioned_plot <- ggplot( data = plot_data, aes(x=sea_level, y=month_date) ) + geom_line() + facet_grid(.~section )
+
+print(sectioned_plot)
+
+
 
