6c5578e0e524d98844d8f00ae7520a41c02f9094
Sangeetha Sasikumar <sangeekumar7@gmail.com>
2022-07-15 22:49:47 -0400

    Update quiz1.r

--- Bridge-Program/quiz1.r
+++ Bridge-Program/quiz1.r
@@ -16,6 +16,11 @@ b
 table(b) #wrong
 nlevels(b) #correct, nlevels: The Number of Levels of a Factor
 #Use R to create a vector that takes the vector from question 1 and performs on it the formula 3𝑥𝑥2 − 4𝑥𝑥 + 1.
-vec<- c(2,3,2,4,5,6,6,7,8,9,10)
+'''vec<- c(2,3,2,4,5,6,6,7,8,9,10)
 for (i in vec)
-  3*i*i*
+  new<-c(3*sqrt(i)-4*i+1)
+print(new) '''
+
+numVec <- c(1:8,7:14,7:10)
+numVec <- 3 * numVec ^ 2 - 4 * numVec + 1
+numVec
