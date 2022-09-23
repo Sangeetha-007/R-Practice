edbd721a498fc6047ed158849b166b79c39d5c5a
Sangeetha Sasikumar <sangeekumar7@gmail.com>
2022-09-23 18:23:57 -0400

    Update datacamp.r

--- Regex Practice/datacamp.r
+++ Regex Practice/datacamp.r
@@ -1,1 +1,16 @@
+# Familiarize yourself with the vector by printing it
+movie_titles
+
+# List all movies that start with "The"
+movie_titles[str_detect(
+  movie_titles,
+  pattern = "^The"
+)]
+
+# List all movies that end with "3D"
+movie_titles[str_detect(
+  movie_titles,
+  pattern = "3D$"
+)]
+
 
